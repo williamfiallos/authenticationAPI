ef892b6fb9146d6c0f94b18ff4c552b16767ba09
Alan Cardero <alan.cardero@lavu.com>
2019-04-08 18:51:01 -0400

    added all changes from API and some authentication

---
+++ routes/auth-routes.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const authRoutes = express.Router();
+
+const passport = require('passport');
+const bcrypt = require('bcryptjs');
+
+// require the user model !!!!
+const User = require('../models/user-model');
+
+authRoutes.post('/signup', (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    res.status(400).json({ message: 'Provide username and password' });
+    return;
+  }
+
+  if (password.length < 7) {
+    res
+      .status(400)
+      .json({
+        message:
+          'Please make your password at least 8 characters long for security purposes.',
+      });
+    return;
+  }
+
+  User.findOne({ username }, (err, foundUser) => {
+    if (err) {
+      res.status(500).json({ message: 'Username check went bad.' });
+      return;
+    }
+
+    if (foundUser) {
+      res.status(400).json({ message: 'Username taken. Choose another one.' });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(10);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const aNewUser = new User({
+      username: username,
+      password: hashPass,
+    });
+
+    aNewUser.save(err => {
+      if (err) {
+        res
+          .status(400)
+          .json({ message: 'Saving user to database went wrong.' });
+        return;
+      }
+
+      // Automatically log in user after sign up
+      // .login() here is actually predefined passport method
+      req.login(aNewUser, err => {
+        if (err) {
+          res.status(500).json({ message: 'Login after signup went bad.' });
+          return;
+        }
+
+        // Send the user's information to the frontend
+        // We can use also: res.status(200).json(req.user);
+        res.status(200).json(aNewUser);
+      });
+    });
+  });
+});
+
+module.exports = authRoutes;
